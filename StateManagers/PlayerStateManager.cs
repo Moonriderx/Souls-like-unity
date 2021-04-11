46c503bad8c61098f55441a656c03ef0eb163dc6
Moonriderx <65513534+Moonriderx@users.noreply.github.com>
2021-04-11 20:13:00 +0300

    Update PlayerStateManager.cs

--- StateManagers/PlayerStateManager.cs
+++ StateManagers/PlayerStateManager.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -71,67 +71,4 @@ namespace Moonrider {
 
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-namespace Moonrider {
 
-    public class PlayerStateManager : CharacterStateManager // all the logic for states in main player will be placed here
-        // because the CharacterStateManager already implements the init we don't have to new Init. 
-    {
-
-        public override void Init()
-        {
-            base.Init();
-
-            State locomotion = new State(
-                new List<StateAction>() // FixedUpdate
-                {
-                },
-                new List<StateAction>() // Update
-                {
-                },
-                new List<StateAction>() // LateUpdate
-                {
-                }
-                );
-
-            State attackState = new State(
-               new List<StateAction>() // FixedUpdate
-                {
-               },
-               new List<StateAction>() // Update
-                {
-               },
-               new List<StateAction>() // LateUpdate
-                {
-               }
-               );
-
-            RegisterState("locomotion", locomotion);
-            RegisterState("attackState", attackState);
-
-            ChangeState("locomotion");
-
-
-        }
-
-        private void FixedUpdate()
-        {
-            base.FixedTick();
-        }
-
-        private void Update()
-        {
-            base.Tick();
-        }
-
-        private void LateUpdate()
-        {
-            base.LateTick();
-        }
-
-    }
-}
->>>>>>> f4397c712eb74e6fe255b617b1f9b747e7f69ffa
