2b8efb44f00195e6f77c8944314dda329c798417
Moonriderx <luben1993@abv.bg>
2021-04-11 14:25:37 +0300

    Added State Actions and State Managers

---
+++ StateManagers/CharacterStateManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Moonrider {
+    public class CharacterStateManager : StateManager
+    {
+        [Header("References")]
+
+        public Animator anim; // every character will have animator
+        public new Rigidbody rigidbody;
+
+
+        [Header("Controller Values")]
+        public float vertical;
+        public float horizontal;
+
+        public override void Init()
+        {
+            anim = GetComponentInChildren<Animator>();
+            rigidbody = GetComponentInChildren<Rigidbody>();
+        }
+    }
+}
---
+++ StateManagers/PlayerStateManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Moonrider {
+
+    public class PlayerStateManager : CharacterStateManager // all the logic for states in main player will be placed here
+        // because the CharacterStateManager already implements the init we don't have to new Init. 
+    {
+
+        public override void Init()
+        {
+            base.Init();
+
+            State locomotion = new State(
+                new List<StateAction>() // FixedUpdate
+                {
+                },
+                new List<StateAction>() // Update
+                {
+                },
+                new List<StateAction>() // LateUpdate
+                {
+                }
+                );
+        }
+
+    }
+}
