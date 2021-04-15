355073f1710d44fac2a1b94cf8cbd0ab4c8cfe65
Moonriderx <luben1993@abv.bg>
2021-04-15 23:17:27 +0300

    Update KeepBool.cs
    Added namespace

--- Old Resources/Scripts/KeepBool.cs
+++ Old Resources/Scripts/KeepBool.cs
@@ -1,26 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class KeepBool : StateMachineBehaviour
+namespace MoonriderOLD
 {
-
-    public string boolName;
-    public bool status;
-    public bool resetOnExit = true;
-
-    
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public class KeepBool : StateMachineBehaviour
     {
 
-        animator.SetBool(boolName, status); // as long as the OnStateUpdate runs (whenever this layer is on this one) it is going to be activating the canMove boolean to true or false
+        public string boolName;
+        public bool status;
+        public bool resetOnExit = true;
+
+
+        override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+
+            animator.SetBool(boolName, status); // as long as the OnStateUpdate runs (whenever this layer is on this one) it is going to be activating the canMove boolean to true or false
+
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (resetOnExit)
+                animator.SetBool(boolName, !status); // change the status to the oposite when we are leaving the state
+        }
 
     }
-
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        if (resetOnExit)
-        animator.SetBool(boolName, !status); // change the status to the oposite when we are leaving the state
-    }
-
 }
